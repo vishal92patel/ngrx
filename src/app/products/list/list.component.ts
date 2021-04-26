d37ce4237c50ea8cbe4f154168f7c68642cfaeb7
vishal92patel <vishal92_patel@yahoo.com>
2021-04-26 16:40:19 +0530

    ++

--- src/app/products/list/list.component.ts
+++ src/app/products/list/list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
 import { Store } from "@ngrx/store";
-import { ProductState, State } from '../state/product.reducer';
+import { getDisplayProductCode, ProductState, State } from '../state/product.reducer';
 
 @Component({
   selector: 'app-list',
@@ -19,9 +19,14 @@ export class ListComponent implements OnInit {
     this.productsService.getProducts().subscribe((res) => {
       this.products = res;
     });
-    this.store.select('products').subscribe((e) => {
-      this.displayProductCode = e.displayProductCode;
-    })
+    // ** Without Selector **
+    // this.store.select('products').subscribe((e) => {
+    //   this.displayProductCode = e.displayProductCode;
+    // });
+    // ** With Selector **
+    this.store.select(getDisplayProductCode).subscribe((res) => {
+      this.displayProductCode = res;
+    });
   }
   onProductDisplayCode(e: any) {
     this.store.dispatch(
--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,6 +1,8 @@
-import { createAction, createReducer, on } from '@ngrx/store';
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from '@ngrx/store';
 import { Product } from '../product';
 
+// We can extends this with appState if exist (ex:),
+// create this to use in component, 
 export interface State {
     products: ProductState
 }
@@ -16,6 +18,22 @@ const initialState: ProductState = {
     currentProduct: null,
     products: []
 }
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getDisplayProductCode = createSelector(
+    getProductFeatureState,
+    state => state.displayProductCode
+);
+export const getCurrentProduct = createSelector(
+    getProductFeatureState,
+    state => state.currentProduct
+);
+export const getProducts = createSelector(
+    getProductFeatureState,
+    state => state.products
+)
+
 
 export const productReducer = createReducer<ProductState>(
     initialState,
